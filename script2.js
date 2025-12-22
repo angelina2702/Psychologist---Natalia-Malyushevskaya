488ed339a99e2fcf8ea798dd64cf096d313a49d5
angelina2702 <angelinamalusevskaa@gmail.com>
2025-12-22 15:15:56 +0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -136,17 +136,19 @@ if (document.attachEvent) {
 	document.onkeydown = handleKeyDown;
 }
 
-const menuBtn = document.getElementById("img2");
-const dropdown = document.getElementById("dropdownMenu");
 
-menuBtn.addEventListener("click", function (event) {
-	event.stopPropagation();
-	dropdown.classList.toggle("open");
-});
 
-// Закрыть меню при клике вне его
-document.addEventListener("click", function (event) {
-	if (!dropdown.contains(event.target) && event.target !== menuBtn) {
-		dropdown.classList.remove("open");
-	}
-});
+ const menuBtn = document.getElementById('menuButton');
+	const dropdown = document.getElementById('dropdownMenu');
+
+	menuBtn.addEventListener('click', function(event) {
+	  event.stopPropagation();
+	  dropdown.classList.toggle('open');
+	});
+
+	// Закрыть меню при клике вне его
+	document.addEventListener('click', function(event) {
+	  if (!dropdown.contains(event.target) && event.target !== menuBtn) {
+		dropdown.classList.remove('open');
+	  }
+	});
